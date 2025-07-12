80ab38bcf63446ed67fd469a082a15822d26e603
Aparna Sasi <156825551+aparnaa001@users.noreply.github.com>
2025-07-12 14:57:47 +0530

    Update settings.py

--- Steganography/settings.py
+++ Steganography/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://*']
+CSRF_TRUSTED_ORIGINS = ['https://cryptic-canvas-uvfr.onrender.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
